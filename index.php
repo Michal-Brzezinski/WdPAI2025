3bbf76a46dac1e178d82443ac93c704fba048e8d
Michal Brzezinski <michal.brzezinski.off@gmail.com>
2025-10-22 08:53:51 +0200

    Kontrolery + zmiany css w login

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require 'Routing.php';
 
-$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = trim($_SERVER['REQUEST_URI'], '/'); // zmienna globalna server
 $path = parse_url($path, PHP_URL_PATH);
 
 // var_dump($path);
