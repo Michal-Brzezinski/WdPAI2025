f7ace177373aac8c94ec3d69852b7d647f903bf5
Michal Brzezinski <michal.brzezinski.off@gmail.com>
2025-11-30 20:21:24 +0100

    lab with login and database integration

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -1,10 +1,19 @@
 <?php
 
 require_once 'AppController.php';
+require_once __DIR__ . "/../repository/UserRepository.php";
 
 // TODO TUTAJ Z TEGO DOKUMENTU GOOGLE DODAĆ DO WŁASNYCH POTRZEB MECHANIZM AUTENTYKACJI
 class SecurityController extends AppController
 {
+
+    private $userRepository;
+
+    public function __construct()
+    {
+        $this->userRepository = new UserRepository();
+    }
+
     // TODO dekarator, który definiuje, jakie metody HTTP są dostępne
     public function login()
     {
@@ -15,11 +24,25 @@ class SecurityController extends AppController
         $email = $_POST["email"] ?? '';
         $password = $_POST["password"] ?? '';
 
+        if (empty($email) || empty($password)) {
+            return $this->render('login', ['message' => 'Fill all fields']);
+        }
+
+        $user = $this->userRepository->getUserByEmail($email);
+
+        if (!$user) {
+            return $this->render('login', ['message' => 'User not found']);
+        }
+
+        if (!password_verify($password, $user['password'])) {
+            return $this->render('login', ['message' => 'Wrong password']);
+        }
         // TODO get data from database
 
-        $this->render("dashboard"); // rozwiazanie z bledem ladowania kart i brakiem zmiany url
-        // header('Location: /dashboard'); // rozwiazanie ze zmiana url i zaladowaniem kart
-        // exit;
+
+        // TODO create user session (żeby wiedzieć czy użytkownik jest zalogowany czy nie)
+
+        return $this->render("dashboard"); // rozwiazanie z bledem ladowania kart i brakiem zmiany url
     }
 
     public function register()
@@ -37,6 +60,25 @@ class SecurityController extends AppController
         $password2 = $_POST["password2"] ?? '';
         $firstname = $_POST["firstname"] ?? '';
         $lastname = $_POST["lastname"] ?? '';
+
+        if (empty($email) || empty($password1) || empty($firstname)) {
+            return $this->render('register', ['message' => 'Fill all fields']);
+        }
+
+        if ($password1 !== $password2) {
+            return $this->render('register', ['message' => 'asswords should be the same!']);
+        }
+
+        // TODO check if user with this email already exists
+
+        $hashedpassword = password_hash($password1, PASSWORD_BCRYPT);
+
+        $this->userRepository->createUser(
+            $email,
+            $hashedpassword,
+            $firstname,
+            $lastname
+        );
 
 
         // TODO insert to database user
--- src/repository/UserRepository.php
+++ src/repository/UserRepository.php
@@ -16,4 +16,31 @@ class UserRepository extends Repository
 
         return $users;
     }
+
+    public function getUserByEmail(string $email)
+    {
+        $stmt = $this->database->connect()->prepare('
+            SELECT * FROM users WHERE email = :email 
+        ');
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+
+        $users = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $users;
+    }
+
+    public function createUser(string $email, string $hashedpassword, string $firstname, string $lastname)
+    {
+        // zrobić to w bloku try-catch i wtedy wywołać komunikat dodane/nie dodano usera
+        $stmt = $this->database->connect()->prepare('
+        INSERT INTO users (email, password, firstName, lastname) VALUES(?,?,?,?);
+        ');
+        $stmt->execute([
+            $email,
+            $hashedpassword,
+            $firstname,
+            $lastname
+        ]);
+    }
 }
